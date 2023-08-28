181811383e411b2d1d61b24f371a36dc853a85e5
Adam Lamine <office@adamlamine.com>
2023-08-28 15:52:07 +0200

    Add gotube.plot imports to __init__.py

--- gotube/__init__.py
+++ gotube/__init__.py
@@ -1,3 +1,5 @@
-from gotube.benchmarks import BaseSystem # noqa
-from gotube.main import run_gotube # noqa
-from gotube.main import add_gotube_args # noqa
+from gotube.benchmarks import BaseSystem  # noqa
+from gotube.main import run_gotube  # noqa
+from gotube.main import add_gotube_args  # noqa
+from gotube.plot import plot_gotube
+from gotube.plot import parse_arguments
